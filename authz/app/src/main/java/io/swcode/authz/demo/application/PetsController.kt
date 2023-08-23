3b59aafaa809c33983cdcc923d05ee72557ea621
Viktor Gottfried <viktor.gottfried@ext.ehealthexperts.de>
2023-08-23 14:37:28 +0200

    add and get pets

--- authz/app/src/main/java/io/swcode/authz/demo/application/PetsController.kt
+++ authz/app/src/main/java/io/swcode/authz/demo/application/PetsController.kt
@@ -3,18 +3,22 @@ package io.swcode.authz.demo.application
 import io.swcode.authz.demo.application.dto.Pet
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PetsController {
+    private val pets = mutableListOf(Pet("dog"))
+
     @GetMapping("/pets")
     fun getPets(): List<Pet> {
-        return listOf(Pet("dog"))
+        return pets
     }
 
-    @GetMapping("/pets2")
+    @PostMapping("/pets")
     @PreAuthorize("hasAnyRole('admin')")
-    fun getPets2(): List<Pet> {
-        return listOf(Pet("dog"))
+    fun addPet(pet: Pet): Pet {
+        pets.add(pet)
+        return pet
     }
 }
